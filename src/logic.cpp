e6fa9de2ffacb8df6c84788e9afdf386adb53716
Susa <kulmala.susa@gmail.com>
2023-09-29 22:26:16 +0300

    Added play bool

--- src/logic.cpp
+++ src/logic.cpp
@@ -9,3 +9,14 @@ Logic::Logic()
     winner = '-';
     draw = false;
 }
+bool Logic::play(unsigned int x, unsigned int y)
+{
+    if (draw or won) return false;
+    if (!(0<=x<=2 and 0<=x<=2)) return false;
+    if (!(grid[x][y]) == '-') return false;
+
+    grid[x][y] = player;
+    if (player == 'X') player = 'O';
+    if (player == 'O') player = 'X';
+    return true;
+}
--- src/logic.hpp
+++ src/logic.hpp
@@ -1,11 +1,23 @@
 #ifndef _LOGIC_
 #define _LOGIC_
 
+#include <array>
+
 class Logic
 {
 public:
     Logic();
+    bool play(unsigned int x, unsigned int y);
 
+private:
+
+
+    std::array<std::array<char, 3>, 3> grid;
+    bool running;
+    char player;
+    bool won;
+    char winner;
+    bool draw;
 };
 
 #endif;
